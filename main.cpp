a022f8262f7ed2dfcedde47fb9cccd5a90ebab65
Rafał Palka <raflikpalka@gmail.com>
2026-03-06 19:09:49 +0100

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
 
-int main() {
-    // Inicjalizacja GLFW
+int main() 
+{
     if (!glfwInit()) return -1;
 
-    // Konfiguracja wersji OpenGL (3.3 Core)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -19,12 +17,10 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
-    // Inicjalizacja GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         return -1;
     }
 
-    // Pętla renderowania
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
